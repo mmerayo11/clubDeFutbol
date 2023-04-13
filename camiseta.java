f58ac4eb4d3b555bc71f89018b9c901c467a237b
Mario <mmerayo1803@gmail.com>
2023-04-13 12:08:45 +0200

    Primer commit

---
+++ camiseta.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class camiseta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class camiseta
+{
+    // instance variables - replace the example below with your own
+    private int dorsal;
+    
+    private String color;
+
+    /**
+     * Constructor for objects of class camiseta
+     */
+    public camiseta(int numero, String colorCami)
+    {
+        int dorsal = numero;
+        String color = colorCami;
+    }
+}
